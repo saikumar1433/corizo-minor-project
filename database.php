83a1cb6a852ad27efa649c78ab27ccbf1263f5bc
saikumar1433 <163817286+saikumar1433@users.noreply.github.com>
2024-03-18 11:58:30 +0530

    Delete database.php

--- database.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "e-commerce");
-        if($connect === false){
-            die("ERROR: could not connect" . mysqli_connect_error());
-        }
-        ?>
